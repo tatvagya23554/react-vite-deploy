c733202a1e992e8da0e92d562dd3d079d1096acf
tatatvagya23554 <tatvagya23554@iiitd.ac.in>
2025-04-20 23:18:50 +0530

    first commit

---
+++ src/components/AnimatedCounter.jsx
@@ -0,0 +1,51 @@
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+import CountUp from "react-countup";
+import { counterItems } from "../constants";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const AnimatedCounter = () => {
+  const counterRef = useRef(null);
+  const countersRef = useRef([]);
+
+  useGSAP(() => {
+    countersRef.current.forEach((counter, index) => {
+      const numberElement = counter.querySelector(".counter-number");
+      const item = counterItems[index];
+      gsap.set(numberElement, { innerText: "0" });
+      gsap.to(numberElement, {
+        innerText: item.value,
+        duration: 2.5,
+        ease: "power2.out",
+        snap: { innerText: 1 },
+        scrollTrigger: {
+          trigger: "#counter",
+          start: "top center",
+        },
+        onComplete: () => {
+          numberElement.textContent = `${item.value}${item.suffix}`;
+        },
+      });
+    }, counterRef);
+  }, []);
+
+  return (
+    <div id="counter" ref={counterRef} className="padding-x-lg xl:mt-0 mt-32">
+      <div className="mx-auto grid-4-cols">
+        {counterItems.map((item, index) => (
+          <div key={index} ref={(el) => el && (countersRef.current[index] = el)}
+            className="bg-zinc-900 rounded-lg p-10 flex flex-col justify-center">
+            <div key={counterItems.label} className="counter-number text-white-50 text-5xl font-bold mb-2">
+              <CountUp suffix={item.suffix} end={item.value} />
+            </div>
+            <div className="text-white-50 text-lg">{item.label}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default AnimatedCounter;
